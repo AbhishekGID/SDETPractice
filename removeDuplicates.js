adb7ec94bada2608c18aa0f71021a1c7dd650cfe
Abhishek <abhi@Abhisheks-MacBook-Air.local>
2026-05-20 22:41:48 +0530

    Using for loop to remove duplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,5 +1,20 @@
-let arr = [1, 2, 2, 3, 1];
-
+// // Remove Duplicates Using Set
+let arr = [1, 2, 2, 4, 4, 4, 4, 4, 3, 1];
 let unique = [...new Set(arr)];
-
 console.log(unique);
+
+//Remove using For loop and Set
+function removeDupes(arr) {
+  let seen = new Set();
+  let dupes = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!seen.has(arr[i])) {
+      seen.add(arr[i]);
+      dupes.push(arr[i]);
+    }
+  }
+  return dupes;
+}
+
+console.log(removeDupes([1, 2, 2, 4, 5, 5, 5, 5, 5, 3, 1]));
