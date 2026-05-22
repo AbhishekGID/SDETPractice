cccc0caadf8a71a7cf2afed8e73e20dad42bfe55
AbhishekGID <ais.abhi19@gmail.com>
2026-05-22 17:16:25 +0530

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,8 @@
 function fibonacci(n) {
-
     let a = 0
     let b = 1
-
     console.log(a)
     console.log(b)
-
     for (let i = 2; i < n; i++) {
 
         let next = a + b
@@ -16,5 +13,16 @@ function fibonacci(n) {
         b = next
     }
 }
-
 fibonacci(17)
+
+
+// Store Fibonacci in Array
+function fibonaccii(n) {
+    let series = [0, 1]
+    for (let i = 2; i < n; i++) {
+        series[i] = series[i - 1] + series[i - 2]
+    }
+    return series
+}
+
+console.log(fibonaccii(20))
