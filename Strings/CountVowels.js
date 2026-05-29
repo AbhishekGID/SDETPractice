1f9ee61ca17c51f6e9c553f645fd12341f9b1190
Abhishek Tyagi <ais.abhi19@gmail.com>
2026-05-29 13:58:39 +0530

    Strings

--- Strings/CountVowels.js
+++ Strings/CountVowels.js
@@ -1,15 +1,11 @@
 function countVowels(str) {
   let count = 0;
-
   let vowels = "aeiou";
-
   for (let char of str.toLowerCase()) {
     if (vowels.includes(char)) {
       count++;
     }
   }
-
   return count;
 }
-
 console.log(countVowels("JavaScript"));
--- Strings/strings.js
+++ Strings/strings.js
@@ -5,6 +5,25 @@
 // includes
 // trim
 
-let name = "ABHISHEK";
-let splitName = name.split("");
-console.log(splitName);
+let Firstname = "ABHISHEK";
+let ReverseName = Firstname.split("").reverse().join("");
+console.log(Firstname);
+console.log(ReverseName);
+let LowerCaseName = Firstname.toLowerCase();
+console.log(LowerCaseName);
+
+for (let char of Firstname) {
+  console.log(char);
+}
+
+// TRIM
+let randomWord = "  HEllo  abhishek";
+console.log(randomWord);
+let trimWord = randomWord.trim("");
+console.log(trimWord);
+
+//Includes
+const searchInput = "Abhishek Tyagi";
+const isMatch = searchInput.includes("Abhishek");
+
+console.log(isMatch); // Output: true
