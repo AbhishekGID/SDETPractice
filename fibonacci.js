6915b3893ce7f7ed402542d648efc0bbc87c7398
Abhishek Tyagi <ais.abhi19@gmail.com>
2026-06-01 18:52:39 +0530

    Fibonacci Practice

--- fibonacci.js
+++ fibonacci.js
@@ -1,28 +1,24 @@
-function fibonacci(n) {
-    let a = 0
-    let b = 1
-    console.log(a)
-    console.log(b)
-    for (let i = 2; i < n; i++) {
-
-        let next = a + b
-
-        console.log(next)
-
-        a = b
-        b = next
-    }
+function fibb(n) {
+  let f1 = 0;
+  let f2 = 1;
+  console.log(f1);
+  console.log(f2);
+  for (i = 2; i <= n; i++) {
+    let next = f1 + f2;
+    console.log(next);
+    f1 = f2;
+    f2 = next;
+  }
 }
-fibonacci(17)
-
+fibb(6);
 
 // Store Fibonacci in Array
 function fibonaccii(n) {
-    let series = [0, 1]
-    for (let i = 2; i < n; i++) {
-        series[i] = series[i - 1] + series[i - 2]
-    }
-    return series
+  let series = [0, 1];
+  for (let i = 2; i < n; i++) {
+    series[i] = series[i - 1] + series[i - 2];
+  }
+  return series;
 }
 
-console.log(fibonaccii(20))
+console.log(fibonaccii(20));
