83b313a0f3fcb450bfbc178078bbe76e21cad281
Abhishek Tyagi <ais.abhi19@gmail.com>
2026-06-03 19:11:26 +0530

    Check Sort Array

---
+++ Arrays/CheckSort.js
@@ -0,0 +1,11 @@
+function isSorted(arr) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isSorted([1, 2, 3, 4, 5]));
+console.log(isSorted([1, 5, 3, 4]));
